add0217ff1d1e8d1a0113f87ba49d480919a2ca1
Augusto Cesar <acs-03@hotmail.com>
2022-12-05 21:13:00 -0300

    ms-cards testing RabbitMQ

--- mscards/src/main/java/com/acsousa/creditcad/mscards/MscardApplication.java
+++ mscards/src/main/java/com/acsousa/creditcad/mscards/MscardApplication.java
@@ -1,11 +1,13 @@
 package com.acsousa.creditcad.mscards;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRabbit
 public class MscardApplication {
 
   public static void main(String[] args) {
